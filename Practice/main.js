5a3bdb174fc802625df00bf3493963c96c6bcb7b
Parker.Wallace <wallace.parker@hotmail.com>
2024-05-27 14:26:23 -0230

    added docu selector example

--- Practice/main.js
+++ Practice/main.js
@@ -1,4 +1,9 @@
+'use strict';
+
 addEventListener("DOMContentLoaded", () => {
     body = document.getElementById("body")
     body.append("Hello, World!")
 })
+
+// psuedo jquery
+const $ = selector => document.querySelector(selector)
