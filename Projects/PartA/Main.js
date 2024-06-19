d72b588642b8eb1112978480be5e42f731c9a60b
Parker.Wallace <wallace.parker@hotmail.com>
2024-06-19 14:36:05 -0230

    added some validation TODO: finish validating form b

--- Projects/PartA/Main.js
+++ Projects/PartA/Main.js
@@ -1,18 +1,34 @@
-
-
 $(document).ready(function () {
+    //main menu event listeners
     $("[name='menu']").prop('checked', false)
     $("[name='menu']").on("change", Handle_manu_button_selection)
     $(".Return_to_main_page_btn").on("click", Handle_return_to_menu)
+
+    //divb event listeners
+    $("form[name='create_boxcar'").on("submit", validate_create_boxcar)
 });
 
 function Handle_manu_button_selection(event) {
     console.log('pressed')
-    $(event.target).parent().toggle()
+    $(this).parent().toggle()
     $(`body div:nth-child(${event.target.getAttribute("data-index")})`).toggle()
 }
 
 function Handle_return_to_menu(event) {
     $(event.target).parent().toggle()
     $("#divA").toggle()
+}
+
+function validate_create_boxcar(event){
+    event.preventDefault();
+    $(this).find("span").text("")
+    let forminfo = {}
+    if (parseInt($("#Max_Gross_Weight_input").val()) < parseInt($("#TAREWeight_input").val())) {
+        console.log("bad input")
+        $("#Max_Gross_Weight_input").next().text("Gross weight must be larger than TARE weight")
+    }
+
+    
+    console.log(forminfo)
+    console.log("form submitted")
 }
