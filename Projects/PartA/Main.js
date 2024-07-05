82815318e36c32d4c956c7466d928f2dfc9a7ebb
parker wallace <wallace.parker@hotmail.com>
2024-07-05 14:09:18 -0230

    added ability to show div D added ability to select box car fox: form validation

--- Projects/PartA/Main.js
+++ Projects/PartA/Main.js
@@ -3,7 +3,7 @@
  * make it such that div c shows udirng the initla div b load if there is data
 */
 
-var Configured_Box_Cars = [GENERATERANDOMBOXCAR(), GENERATERANDOMBOXCAR()]
+var CONFIGUREDBOXCARS = [GENERATERANDOMBOXCAR(), GENERATERANDOMBOXCAR()]
 // var Configured_Box_Cars = []
 
 function boxcar(ID, tareWeight, maxGrossWeight, cargoWeight, grossWeight) {
@@ -31,60 +31,61 @@ function Handle_return_to_menu() {
 
 }
 
-/*
- * validation for the create box car form that ensures:
- * 
- * Boxcar ID is in the form BX followed by 3 digits
- * TARE weight is between 0 and 200000
- * Max gross weight is Greater than TARE weight and between 0 and 200000 
- */
-function validate_create_boxcar(event){
-    //prevent default and clear any existing error messages
-    event.preventDefault();
-    $(this).find("span").text("")
-    
-    if (!(/BX\d{3}/).test($("#Boxcar_ID_input").val())) {
-        $("#Boxcar_ID_input").next().text("Boxcar ID must be in the format BX123")
-    }
-    else if (0 > parseInt($("#TAREWeight_input").val()) || parseInt($("#TAREWeight_input").val()) > 20000) {
-        $("#TAREWeight_input").next().text("TARE weight ust be between 0 and 200000")
-    }
-    else if (parseInt($("#Max_Gross_Weight_input").val()) < parseInt($("#TAREWeight_input").val())) {
-        $("#Max_Gross_Weight_input").next().text("Gross weight must be larger than TARE weight")
-    }  
-    else if (0 > parseInt($("#Max_Gross_Weight_input").val()) || parseInt($("#Max_Gross_Weight_input").val()) > 20000) {
-        $("#Max_Gross_Weight_input").next().text("Max gross weight must be between 0 and 200000")}
-    // if all conditions pass add the box car to the array
-    else {
 
-        Configured_Box_Cars.push(new boxcar(
-        $(this[0]).val(),
-        $(this[1]).val(),
-        $(this[2]).val(),
-        $(this[3]).val(),
-        $(this[4]).val()
-        ))
-        DisplayDivC()
-    }
-}
 
 function DisplayDivB () {
-    $("#divA").hide();
-    $("#divB").show();
+    $("#divA, #divB").toggle();
     $("form").on("submit", validate_create_boxcar)
     DisplayDivC();
+    
+    /*
+    * validation for the create box car form that ensures:
+    * 
+    * Boxcar ID is in the form BX followed by 3 digits
+    * TARE weight is between 0 and 200000
+    * Max gross weight is Greater than TARE weight and between 0 and 200000 
+    */
+    function validate_create_boxcar(event){
+        //prevent default and clear any existing error messages
+        event.preventDefault();
+        $(this).find("span").text("")
+        
+        if (!(/BX\d{3}/).test($("#Boxcar_ID_input").val())) {
+            $("#Boxcar_ID_input").next().text("Boxcar ID must be in the format BX123")
+        }
+        else if (0 > parseInt($("#TAREWeight_input").val()) || parseInt($("#TAREWeight_input").val()) > 20000) {
+            $("#TAREWeight_input").next().text("TARE weight ust be between 0 and 200000")
+        }
+        else if (parseInt($("#Max_Gross_Weight_input").val()) < parseInt($("#TAREWeight_input").val())) {
+            $("#Max_Gross_Weight_input").next().text("Gross weight must be larger than TARE weight")
+        }  
+        else if (0 > parseInt($("#Max_Gross_Weight_input").val()) || parseInt($("#Max_Gross_Weight_input").val()) > 20000) {
+            $("#Max_Gross_Weight_input").next().text("Max gross weight must be between 0 and 200000")}
+        // if all conditions pass add the box car to the array
+        else {
+
+            CONFIGUREDBOXCARS.push(new boxcar(
+            $(this[0]).val(),
+            $(this[1]).val(),
+            $(this[2]).val(),
+            $(this[3]).val(),
+            $(this[4]).val()
+            ))
+            DisplayDivC()
+        }
+}
 }
 
 function DisplayDivC() {
     divCTableBody = $("#divC").find("tbody")
     divCTableFooter = $("#divC").find("tfoot")
-    if (Configured_Box_Cars.length > 0) {
+    if (CONFIGUREDBOXCARS.length > 0) {
         $("#divC").show();
         divCTableBody.empty()
         divCTableFooter.empty()
-        console.log(Configured_Box_Cars)
+        console.log(CONFIGUREDBOXCARS)
         let totalCargoWeight = 0;
-        Configured_Box_Cars.forEach(Boxcar => {
+        CONFIGUREDBOXCARS.forEach(Boxcar => {
             divCTableBody.append(`<tr>`);
             divCTableBody.append(`<td>${Boxcar.ID}</td>`);
             divCTableBody.append(`<td>${Boxcar.tareWeight}</td>`); 
@@ -98,22 +99,27 @@ function DisplayDivC() {
 } 
 
 function DisplayDivD () {
-    $("#divA").hide();
-    $("#divD").show();
+    $("#divA, #divD").toggle();
+    let boxCarSelectedValue = $("#Box_Car_Selected_input");
     $("#cargoForm :input").prop("disabled", true);
     divDTableBody = $("#divD").find("tbody");
-    divDTableBody.addClass("selectabletable")
-    divDTableBody.empty()
-    Configured_Box_Cars.forEach(Boxcar => {
-        divDTableBody.append(document.createElement("tr"))
-        divDTableBody.append(`<td>${Boxcar.ID}</td>`);
-    });
-    
-    $("td").on("click", ()=>{
+    divDTableBody.addClass("selectabletable");
+    divDTableBody.empty();
+    CONFIGUREDBOXCARS.forEach(Boxcar => {
+        divDTableBody.append(document.createElement("tr"));
+        let boxcarIdCell = document.createElement("td");
+
+        $(boxcarIdCell).on('click', ()=>{   
         divDTableBody.prop("disabled", true);
-        divDTableBody.removeClass("selectabletable")
+        divDTableBody.removeClass("selectabletable");
         $("#cargoForm :input").prop("disabled", false);
+        boxCarSelectedValue.val(Boxcar.ID)
+        $("td").off()
     })
+        boxcarIdCell.textContent = Boxcar.ID
+        divDTableBody.append(boxcarIdCell);
+    });
+    function validateAddCargo () {}
 
 }
 
